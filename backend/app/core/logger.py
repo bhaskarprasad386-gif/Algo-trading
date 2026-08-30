75a05c023ce2c867035350d53a63891c40800732
bhaskarprasad386-gif <bhaskarprasad386@gmail.com>
2026-08-30 15:24:07 +0530

    Add Loguru logger setup

---
+++ backend/app/core/logger.py
@@ -0,0 +1,15 @@
+import sys
+from loguru import logger
+
+logger.remove()
+
+logger.add(
+    sys.stdout,
+    colorize=True,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    level="DEBUG"
+)
+
+logger.add("backend_logs.log", rotation="10 MB", retention="10 days", level="INFO")
+
+app_logger = logger
