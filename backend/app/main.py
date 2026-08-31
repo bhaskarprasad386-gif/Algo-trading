53a6c9d2db56f03f1b3cd544ef07f1867fec6696
bhaskarprasad386-gif <bhaskarprasad386@gmail.com>
2026-08-31 08:34:49 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query, HTTPException
 from app.instruments.routes import router as instruments_router
 from app.strategy_engine.routes import router as arbitrage_router
+from app.order_engine.routes import router as orders_router
 
 from app.core.config import settings
 from app.core.logger import app_logger
@@ -22,6 +23,8 @@ app = FastAPI(
     version="0.1.0",
     debug=settings.debug,
 )
+app.include_router(orders_router)
+
 app.include_router(arbitrage_router)
 
 app.include_router(instruments_router)
