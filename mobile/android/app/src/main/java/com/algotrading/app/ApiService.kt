13d0b71f443c1408b2479d1cf732466cecd72690
bhaskarprasad386-gif <bhaskarprasad386@gmail.com>
2026-08-31 10:03:52 +0530

    Update ApiService.kt

--- mobile/android/app/src/main/java/com/algotrading/app/ApiService.kt
+++ mobile/android/app/src/main/java/com/algotrading/app/ApiService.kt
@@ -2,7 +2,9 @@ package com.algotrading.app
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 data class MarketStatus(
@@ -19,12 +21,27 @@ data class QuoteData(
     val spread: Double
 )
 
+data class OrderRequest(
+    val symbol: String,
+    val quantity: Int,
+    val transactionType: String // "BUY" or "SELL"
+)
+
+data class OrderResponse(
+    val orderId: String,
+    val status: String,
+    val message: String
+)
+
 interface ApiInterface {
     @GET("/")
     suspend fun getRootStatus(): MarketStatus
 
     @GET("/api/v1/quote")
     suspend fun getBidAskQuote(@Query("symbol") symbol: String): QuoteData
+
+    @POST("/api/v1/order")
+    suspend fun placeOrder(@Body request: OrderRequest): OrderResponse
 }
 
 object ApiService {
