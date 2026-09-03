f61458d683509af96482532cf43cb86c330d098b
bhaskarprasad386-gif <bhaskarprasad386@gmail.com>
2026-09-03 13:49:34 +0530

    feat: add broker settings navigation helper

---
+++ web/dashboard/broker-settings.js
@@ -0,0 +1,4 @@
+// Broker Settings entry helper. Dashboard can call openBrokerSettings() from its Settings action.
+window.openBrokerSettings = function () {
+  window.location.href = '/dashboard/broker';
+};
